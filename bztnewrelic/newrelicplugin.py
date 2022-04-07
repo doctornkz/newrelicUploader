8b038e667189bedc202e0f17462fd68f4671f72c
Oles Pisarenko <oles.pisarenko@olx.com>
2022-04-07 18:43:28 +0100

    Fix account

--- bztnewrelic/newrelicplugin.py
+++ bztnewrelic/newrelicplugin.py
@@ -249,16 +249,14 @@ class NewRelicUploader(Reporter, AggregatorListener, Singletone):
         self._dashboard.account_id = self.account_id
         self._dashboard.client_init()
 
-
-
         if self.account_id is '':
-            if self._dashboard.get_account_id() == '':
+            self.account_id = self._dashboard.get_account_id()
+            if self.account_id == '':
                 self.log.warning('NewRelic AccountId is not set. Please check `account-id` config setting ')
                 self.log.warning('Dashboard generator is disabled.')
                 self.dashboard_generator_on = False
             else: 
                 self._dashboard.account_id = self.account_id
-
 
         if not self._dashboard.api_check():
             self.log.warning('NewRelic API token is wrong or enpoint is not correct.')
